a6b5307d1706667ba105f40e8733c5c480847fc4
Trevor Smith <trevor.smith1667@gmail.com>
2021-03-22 22:54:51 -0700

    finished caesar cipher

--- rspec_exercise_1/lib/part_2.rb
+++ rspec_exercise_1/lib/part_2.rb
@@ -9,3 +9,30 @@ def hipsterfy(word)
     end   
     word    
 end 
+
+def vowel_counts(string)
+    vowel_hash = Hash.new(0)
+    vowels = "aeiou"
+    string.each_char do |char| 
+        if vowels.include?(char.downcase)
+            vowel_hash[char.downcase] += 1
+        end 
+    end 
+  vowel_hash
+end 
+
+def caesar_cipher(message, n)
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    cipher = ""
+    message.each_char do |char| 
+        if alphabet.include?(char)
+        old_index = alphabet.index(char)
+        new_index = (old_index + n) % 26
+        cipher += alphabet[new_index]
+        else 
+            cipher += char
+        end
+    end 
+
+    cipher
+end 
